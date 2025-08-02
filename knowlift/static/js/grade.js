bdd1e774d2dbed0c369b95662e16a05453e6a439
Marius Mucenicu <marius_mucenicu@yahoo.com>
2025-08-02 17:06:43 +0300

    Extend the file list to which we add the license headers
    Resolves #252

--- knowlift/static/js/grade.js
+++ knowlift/static/js/grade.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 (C) Marius Mucenicu
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+
 function validateForm(event) {
     const isRadioChecked = document.querySelector('input:checked') !== null;
     const choiceBtn = document.querySelector('#js-btn-choice');
