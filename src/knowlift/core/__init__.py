08384fe34d92a0affb0e920af31ba8c8f0f4e30f
Marius Mucenicu <marius_mucenicu@yahoo.com>
2025-08-25 13:00:18 +0300

    Make core a package

---
+++ src/knowlift/core/__init__.py
@@ -0,0 +1,16 @@
+"""
+Core package for configuration and environment management.
+
+This package provides configuration and environment-specific resources required
+for application startup and runtime.
+
+Modules
+-------
+- settings: Store environment-based configuration and settings.
+
+Miscellaneous objects
+---------------------
+Except for the public objects exported by this module and their public APIs
+(if applicable), everything else is an implementation detail and should not be
+relied upon, as it may change over time.
+"""
